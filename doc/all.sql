d62292367e147727d761d0039385a67d2f89e0c5
张诚 <zhangcheng@sinotrans.com>
2024-07-22 16:00:16 +0800

    3-5 集成mybatis generator,生成demo代码

--- doc/all.sql
+++ doc/all.sql
@@ -8,3 +8,9 @@ create table `test`
 
 
 insert into `test` (`id`,`name`, `password`) values ('1','admin', '123456');
+
+create table `demo`(
+                       `id` bigint auto_increment not null comment 'id',
+                       `name` varchar(50) not null comment '名称',
+                       primary key (`id`)
+) engine = innodb default charset = utf8mb4 comment = '测试';
