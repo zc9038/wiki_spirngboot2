cb56ef19795a554171a4ca94d61141b42aa44dc7
张诚 <zhangcheng@sinotrans.com>
2024-07-24 10:17:35 +0800

    3-7 改造EbookService的list方法，接受Request参数，并返回相应的EbookResp列表。

--- src/main/java/com/zc/wiki_springboot2/controller/EbookController.java
+++ src/main/java/com/zc/wiki_springboot2/controller/EbookController.java
@@ -1,7 +1,9 @@
 package com.zc.wiki_springboot2.controller;
 
 import com.zc.wiki_springboot2.domain.Ebook;
+import com.zc.wiki_springboot2.req.EbookReq;
 import com.zc.wiki_springboot2.resp.CommonResp;
+import com.zc.wiki_springboot2.resp.EbookResp;
 import com.zc.wiki_springboot2.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +31,19 @@ public class EbookController {
     public CommonResp selectByName(String name){
         CommonResp<List<Ebook>> resp = new CommonResp<>();
         List<Ebook> ebookList = ebookService.selectByName(name);
+        resp.setContent(ebookList);
+        return resp;
+    }
+
+    @GetMapping("/listByReq")
+    public CommonResp selectByName(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> ebookList = ebookService.list(req);
+/*        if (ebookList.isEmpty()) {
+            resp.setSuccess(false);
+        } else {
+            resp.setSuccess(true);
+        }*/
         resp.setContent(ebookList);
         return resp;
     }
--- src/main/java/com/zc/wiki_springboot2/service/EbookService.java
+++ src/main/java/com/zc/wiki_springboot2/service/EbookService.java
@@ -3,9 +3,13 @@ package com.zc.wiki_springboot2.service;
 import com.zc.wiki_springboot2.domain.Ebook;
 import com.zc.wiki_springboot2.domain.EbookExample;
 import com.zc.wiki_springboot2.mapper.EbookMapper;
+import com.zc.wiki_springboot2.req.EbookReq;
+import com.zc.wiki_springboot2.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,4 +27,19 @@ public class EbookService {
         criteria.andNameLike("%" + name + "%");
         return ebookMapper.selectByExample(ebookExample);
     }
+
+    public List<EbookResp> list(EbookReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        List<EbookResp> respList = new ArrayList<EbookResp>();
+        for (Ebook ebook : ebookList) {
+            EbookResp resp = new EbookResp();
+            BeanUtils.copyProperties(ebook, resp);
+            respList.add(resp);
+        }
+        return respList;
+    }
+
 }
