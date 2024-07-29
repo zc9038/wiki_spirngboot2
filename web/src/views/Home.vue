e311de0707027d62e4f56eb71431a0558dc1bda6
张诚 <zhangcheng@sinotrans.com>
2024-07-29 10:47:51 +0800

    5-4 集成图标组件,使用样例List样式

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -47,39 +47,69 @@
   </a-layout-sider>
     <a-layout-content
       :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
-      <pre>
-        {{ebooks}}
-        {{ebooks2}}
-      </pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <template #footer>
+          <div>
+            <b>ant design vue</b>
+            footer part
+          </div>
+        </template>
+        <template #renderItem="{ item }">
+          <a-list-item key="item.title">
+            <template #actions>
+          <span v-for="{ icon, text } in actions" :key="icon">
+            <component :is="icon" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+            <template #extra>
+              <img
+                  width="272"
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.title }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.avatar" /></template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
   </a-layout-content>
   </a-layout>
 </template>
 
-<script lang="ts">
-import { defineComponent,onMounted,ref,reactive,toRef} from 'vue';
-import axios from 'axios';
+<script lang="ts" setup>
+import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
+const listData: Record<string, string>[] = [];
 
-export default defineComponent({
-  name: 'TheHeader',
-  setup() {
-    console.log('setup');
-    const ebooks = ref();
-    const ebooks1 = reactive({books:[]});
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
 
-    onMounted(() => {
-      axios.get('http://localhost:8880/ebook/listByReq?name=Spring').then(resp => {
-        const data = resp.data;
-        ebooks.value = data.content;
-        ebooks1.books = data.content;
-        console.log(resp);
-      });
-    });
-    return {
-      ebooks,
-      ebooks2 : toRef(ebooks1,'books')
-    };
-  }
-});
+const pagination = {
+  onChange: (page: number) => {
+    console.log(page);
+  },
+  pageSize: 3,
+};
+const actions: Record<string, any>[] = [
+  { icon: StarOutlined, text: '156' },
+  { icon: LikeOutlined, text: '156' },
+  { icon: MessageOutlined, text: '2' },
+];
 </script>
 
 <style scoped>
